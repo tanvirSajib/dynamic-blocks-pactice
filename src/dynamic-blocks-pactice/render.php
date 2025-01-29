92c7093d3a6fdb9311d54fe8e68de9fe590d14b1
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-29 15:36:18 +0600

    80. Displaying the Latest Posts in the Frond-end Using PHP

--- src/dynamic-blocks-pactice/render.php
+++ src/dynamic-blocks-pactice/render.php
@@ -1,4 +1,33 @@
 <?php
 
-echo "<h1>Dynamic Content</h1>"; 
-var_dump($attributes);
+
+$args = array(
+    'posts_per_page' => $attributes['numberOfPosts'],
+    'post_status' => 'publish'
+);
+$recent_posts = get_posts($args);
+
+$posts = '<ul ' . get_block_wrapper_attributes() . '>';
+foreach($recent_posts as $post) {
+    $title = get_the_title($post);
+    $title = $title ? $title : __('(No title)','latest-posts');
+    $permalink = get_permalink( $post );
+    $excerpt = get_the_excerpt( $post );
+
+    $posts .= '<li>';
+
+    if($attributes["displayFeaturedImage"] && has_post_thumbnail( $post )) {
+        $posts .= get_the_post_thumbnail( $post, 'large' );
+    }
+    $posts .= '<h5><a href="' . esc_url($permalink) . '">' . $title . '</a></h5>';
+    $posts .= '<time datetime="' . esc_attr( get_the_date('c', $post)) . '">' . esc_html( get_the_date('', $post)) . '</time>';
+
+    if(!empty($excerpt)) {
+        $posts .= '<p>' . $excerpt . '</p>';
+    }
+
+    $posts .= '</li>';
+}
+$posts .= '</ul>';
+
+echo $posts;
