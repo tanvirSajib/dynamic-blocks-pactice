2453c087c2833c7a5c7e167bce7fec8a3b28172d
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-29 16:08:12 +0600

    (d/a) 84. Adding Options for Number of Posts and Featured Image

--- src/dynamic-blocks-pactice/edit.js
+++ src/dynamic-blocks-pactice/edit.js
@@ -2,11 +2,12 @@ import { __ } from '@wordpress/i18n';
 import { RawHTML } from '@wordpress/element';
 // eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 import { format, dateI18n, __experimentalGetSettings } from '@wordpress/date';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, ToggleControl, QueryControls } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import './editor.scss';
 
-export default function Edit({ attributes }) {
+export default function Edit({ attributes, setAttributes }) {
 	const { numberOfPosts, displayFeaturedImage } = attributes;
 	const posts = useSelect(
 		(select) => {
@@ -17,51 +18,80 @@ export default function Edit({ attributes }) {
 		},
 		[numberOfPosts]
 	);
+
+	const onDisplayFeaturedImageChange = (value) => {
+		setAttributes({ displayFeaturedImage: value });
+	};
+	const onNumberOfItemsChange = (value) => {
+		setAttributes({ numberOfPosts: value });
+	};
 	return (
-		<ul {...useBlockProps()}>
-			{posts &&
-				posts.map((post) => {
-					const featuredImage =
-						post._embedded &&
-						post._embedded['wp:featuredmedia'] &&
-						post._embedded['wp:featuredmedia'].length > 0 &&
-						post._embedded['wp:featuredmedia'][0];
-					return (
-						<li key={post.id}>
-							{displayFeaturedImage && featuredImage && (
-								<img
-									src={
-										featuredImage.media_details.sizes.large
-											.source_url
-									}
-									alt={featuredImage.alt_text}
-								/>
-							)}
-							<h5>
-								<a href={post.link}>
-									{post.title.rendered ? (
-										<RawHTML>{post.title.rendered}</RawHTML>
-									) : (
-										__('(No title)', 'latest-posts')
-									)}
-								</a>
-							</h5>
-							{post.date_gmt && (
-								<time dateTime={format('c', post.date_gmt)}>
-									{dateI18n(
-										__experimentalGetSettings().formats
-											.date,
-										post.date_gmt
-									)}
-								</time>
-							)}
-							{post.excerpt.rendered && (
-								<RawHTML>{post.excerpt.rendered}</RawHTML>
-							)}
-						</li>
-					);
-				})}
-		</ul>
+		<>
+			<InspectorControls>
+				<PanelBody>
+					<ToggleControl
+						label={__('Display Featured Image', 'latest-posts')}
+						checked={displayFeaturedImage}
+						onChange={onDisplayFeaturedImageChange}
+					/>
+					<QueryControls
+						numberOfItems={numberOfPosts}
+						onNumberOfItemsChange={onNumberOfItemsChange}
+						maxItems={10}
+						minItems={1}
+						orderBy=""
+						onOrderByChange={(value) => console.log(value)}
+						order=""
+						onOrderChange={(value) => console.log(value)}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<ul {...useBlockProps()}>
+				{posts &&
+					posts.map((post) => {
+						const featuredImage =
+							post._embedded &&
+							post._embedded['wp:featuredmedia'] &&
+							post._embedded['wp:featuredmedia'].length > 0 &&
+							post._embedded['wp:featuredmedia'][0];
+						return (
+							<li key={post.id}>
+								{displayFeaturedImage && featuredImage && (
+									<img
+										src={
+											featuredImage.media_details.sizes
+												.large.source_url
+										}
+										alt={featuredImage.alt_text}
+									/>
+								)}
+								<h5>
+									<a href={post.link}>
+										{post.title.rendered ? (
+											<RawHTML>
+												{post.title.rendered}
+											</RawHTML>
+										) : (
+											__('(No title)', 'latest-posts')
+										)}
+									</a>
+								</h5>
+								{post.date_gmt && (
+									<time dateTime={format('c', post.date_gmt)}>
+										{dateI18n(
+											__experimentalGetSettings().formats
+												.date,
+											post.date_gmt
+										)}
+									</time>
+								)}
+								{post.excerpt.rendered && (
+									<RawHTML>{post.excerpt.rendered}</RawHTML>
+								)}
+							</li>
+						);
+					})}
+			</ul>
+		</>
 	);
 }
-
