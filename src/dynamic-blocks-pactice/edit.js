ef29a528d575c9d853863d6b78cc2374165d40c8
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-29 15:52:13 +0600

    82. Displaying the Latest Posts in the Editor

--- src/dynamic-blocks-pactice/edit.js
+++ src/dynamic-blocks-pactice/edit.js
@@ -1,10 +1,13 @@
 import { __ } from '@wordpress/i18n';
+import { RawHTML } from '@wordpress/element';
+// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+import { format, dateI18n, __experimentalGetSettings } from '@wordpress/date';
 import { useBlockProps } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import './editor.scss';
 
 export default function Edit({ attributes }) {
-	const { numberOfPosts } = attributes;
+	const { numberOfPosts, displayFeaturedImage } = attributes;
 	const posts = useSelect(
 		(select) => {
 			return select('core').getEntityRecords('postType', 'post', {
@@ -14,10 +17,50 @@ export default function Edit({ attributes }) {
 		},
 		[numberOfPosts]
 	);
-	console.log(posts);
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<ul {...useBlockProps()}>
+			{posts &&
+				posts.map((post) => {
+					const featuredImage =
+						post._embedded &&
+						post._embedded['wp:featuredmedia'] &&
+						post._embedded['wp:featuredmedia'].length > 0 &&
+						post._embedded['wp:featuredmedia'][0];
+					return (
+						<li key={post.id}>
+							{displayFeaturedImage && featuredImage && (
+								<img
+									src={
+										featuredImage.media_details.sizes.large
+											.source_url
+									}
+									alt={featuredImage.alt_text}
+								/>
+							)}
+							<h5>
+								<a href={post.link}>
+									{post.title.rendered ? (
+										<RawHTML>{post.title.rendered}</RawHTML>
+									) : (
+										__('(No title)', 'latest-posts')
+									)}
+								</a>
+							</h5>
+							{post.date_gmt && (
+								<time dateTime={format('c', post.date_gmt)}>
+									{dateI18n(
+										__experimentalGetSettings().formats
+											.date,
+										post.date_gmt
+									)}
+								</time>
+							)}
+							{post.excerpt.rendered && (
+								<RawHTML>{post.excerpt.rendered}</RawHTML>
+							)}
+						</li>
+					);
+				})}
+		</ul>
 	);
 }
